3c1f4330b8a9a17c19425a43c7bb5e6fc2c52ebb
Oliver Faircliff <faircliff.oliver@gmail.com>
2015-06-01 22:59:34 +0100

    A window

--- main.cpp
+++ main.cpp
@@ -1,31 +1,48 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <cstdlib>
 
 
-int main() {
-    glfwInit();
+const int WIDTH = 640;
+const int HEIGHT = 480;
+
+
+int main(int argc, char** argv) {
+
+    // Setup Stuff
+    if (!glfwInit()) {
+        return EXIT_FAILURE;
+    }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Mah Window!", nullptr, nullptr); // Windowed
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Mah Window!", nullptr, nullptr); // Windowed
+
+    if (!window) {
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
 
     glfwMakeContextCurrent(window);
 
     glewExperimental = GL_TRUE;
     glewInit();
 
+
+    // Other Stuff
     GLuint vertexBuffer;
     glGenBuffers(1, &vertexBuffer);
-
     printf("%u\n", vertexBuffer);
 
     while(!glfwWindowShouldClose(window)) {
+
+        glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -35,6 +52,7 @@ int main() {
         }
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
 }
